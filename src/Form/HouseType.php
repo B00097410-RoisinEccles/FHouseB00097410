955cfa2e65cfc7010b4fe2c25fa84e83501f9ef7
Roisin <B00097410@mytudublin.ie>
2020-08-05 16:29:49 +0100

    generated crud for House entity

---
+++ src/Form/HouseType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\House;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class HouseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('Sponsor')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => House::class,
+        ]);
+    }
+}
