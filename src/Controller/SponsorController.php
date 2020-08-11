ffd77261f00a47c659be9f0d83e9609403622105
Roisin <B00097410@mytudublin.ie>
2020-08-11 17:36:00 +0100

    updating sponsor controller to live under the /admin url prefix

--- src/Controller/SponsorController.php
+++ src/Controller/SponsorController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/sponsor")
+ * @Route("/admin/sponsor")
  */
 class SponsorController extends AbstractController
 {
